299dfe47f624ccdac1e3359012e1374ed4f3cadc
Patrick Gallagher <paddy.gallagher@gmail.com>
2016-11-16 13:49:49 +0000

    Adding Ring dependency. Creating main entry point for the application.

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "http://hapi.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [ring "1.5.0"]]
+  :main hapi.core)
--- src/hapi/core.clj
+++ src/hapi/core.clj
@@ -1,6 +1,10 @@
-(ns hapi.core)
+(ns hapi.core
+  (:require [ring.adapter.jetty :as jetty]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn app [req]
+  {:headers {}
+   :status 200
+   :body "Hello, World!"})
+
+(defn -main []
+  (jetty/run-jetty app {:port 8080}))
