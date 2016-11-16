2e4ced5169c1748dbf20cb79b675c70cfbd9a950
Patrick Gallagher <paddy.gallagher@gmail.com>
2016-11-16 14:05:23 +0000

    Adding dependencies for hugsql and postgres

--- project.clj
+++ project.clj
@@ -4,5 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [ring "1.5.0"]]
+                 [ring "1.5.0"]
+                 [com.layerware/hugsql "0.4.7"]
+                 [org.postgresql/postgresql "9.4.1207"]]
   :main hapi.core)
