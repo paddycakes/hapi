be0ed46518e55fa87093c88d0d122f5b312232ef
Patrick Gallagher <paddy.gallagher@gmail.com>
2016-11-16 16:39:06 +0000

    Using HugSQL to externalize SQL into separate file but still make the SQL available as Clojure functions

--- src/hapi/core.clj
+++ src/hapi/core.clj
@@ -1,5 +1,14 @@
 (ns hapi.core
-  (:require [ring.adapter.jetty :as jetty]))
+  (:require [ring.adapter.jetty :as jetty]
+            [hugsql.core :as hugsql]))
+
+;; (def db (System/getenv "DATABASE_URL"))
+(def db "jdbc:postgres://localhost/hapi")
+
+(hugsql/def-db-fns "hapi/sql/database.sql")
+
+(defn create-endpoint [db]
+  (:id (create-endpoint* db)))
 
 (defn app [req]
   {:headers {}
