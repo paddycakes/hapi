1edca95201deca7410bf2d7852433695ffac325a
Patrick Gallagher <paddy.gallagher@gmail.com>
2016-11-16 12:51:29 +0000

    Initial project import

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject hapi "0.1.0-SNAPSHOT"
+  :description "RESTful web application in Clojure/ClojureScript"
+  :url "http://hapi.com"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
